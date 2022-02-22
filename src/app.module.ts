7fde83b50ec51e4ac3b7cc7c7c33f90c868ecb7d
MarcossHT <mppiress@gmail.com>
2022-02-22 15:21:33 -0300

    deleting unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/27017'), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://marcosht:mongo123@localhost:27017/?authSource=admin&readPreference=primary&ssl=false'), UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
