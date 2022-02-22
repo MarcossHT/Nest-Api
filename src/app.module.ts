60e3491a358c945a69e0fb461bcddf0d124fec81
MarcossHT <mppiress@gmail.com>
2022-02-22 09:46:24 -0300

    Configuring Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/27017'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
